6f496347f9c3b2d90079a1fe592ed9e4e219e825
Pedro Gomes <ph631713@gmail.com>
2026-08-27 20:35:32 -0300

    adding a code

---
+++ calculator.py
@@ -0,0 +1,20 @@
+num1 = int(input("primeiro numero: "))
+num2 = int(input("segundo numero: "))
+
+
+
+if num1 != int(num1):
+    int(input("Digite um número inteiro válido para o primeiro número: "))
+else:
+    char = input("Escolha a conta: ")
+
+
+
+if char == "+":
+    print(num1 + num2)
+elif char == "-":
+    print(num1 - num2)
+elif char == "*":
+    print(num1 * num2)
+elif char == "/":
+    print(num1 / num2)
