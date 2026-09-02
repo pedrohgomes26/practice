353a8cbcf717419512dadf67627424f275a189c5
Pedro Gomes <ph631713@gmail.com>
2026-09-01 21:58:41 -0300

    Fatorial de um numero

---
+++ fatorial.py
@@ -0,0 +1,15 @@
+nf = float(input("digite o numero para fazer o fatorial") )
+n = nf - 1
+n2 = n - 1
+ct = n
+
+while(ct > 0):
+
+    r1 = n * (n - 1)
+    n2 = n * (n - 1)
+
+
+    ct = ct - 1
+    
+
+print(r1)
