5ccc02270b2c53cd128a1575c05f308f75a2d40e
Pedro Gomes <ph631713@gmail.com>
2026-09-14 18:35:43 -0300

    adding feat(script) senha with if else statement

--- js/main.js
+++ js/main.js
@@ -1,21 +1,16 @@
-let idade = Number(prompt("Qual a sua idade?"));
+let tentativas = 3
+let senha = prompt("Digite a sua senha: ")
+console.log(senha)
 
-if (idade >= 18) {
-    alert("Você é maior de idade.");
-} else if (idade < 18) {
-    alert("Você é menor de idade.");
-}
+let entrar = prompt("Coloque a sua senha: ")
 
-let temperatura = Number(prompt("Qual a temperatura esta agora?"));
-
-if (temperatura <= 19) {
-    alert("Ta frio");
-} else if (temperatura >= 20) {
-    alert("Ta normal");
-} else if (temperatura >= 27) {
-    alert("Ta calor");
-} else if (temperatura >= 30) {
-    alert("ta muito calor");
-} else {
-    alert("calor do cão");
+while (tentativas > 0) {
+    if (entrar !== senha) {
+        tentativas = tentativas - 1
+        alert("Senha incorreta! Você tem " + tentativas + " tentativas restantes.")
+        entrar = prompt("Coloque a sua senha: ")
+    } else {
+        alert("Senha correta! Bem-vindo!")
+        break
+    }
 }
